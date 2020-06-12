4ba8c614d7f2bf482587188a27878a2d0f35e334
njkrichardson <nrichardson@g.hmc.edu>
2020-06-11 20:57:03 -0400

    add base distribution

--- setup.py
+++ setup.py
@@ -18,5 +18,8 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.6"
+    python_requires=">=3.6.10", 
+    install_requires = [
+        'numpy', 
+    ]
 )
