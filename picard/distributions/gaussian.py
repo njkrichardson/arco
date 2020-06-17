7fb7b6d7ea451791485e2cf30cefda0781e28682
njkrichardson <nrichardson@g.hmc.edu>
2020-06-16 22:05:06 -0500

    cracked a little bit of a nut related to posterior param updates (see notes)
    Notes
    -------
     * For now at least, I think it makes most sense to just have the priors
    absorb data and a likelihood (which will induce internal parameter
    updates)
     * I may be playing with fire, but for the moment Im going to keep this
    ordering hack in the niw absorb method (l 149 in gaussian.py). At least
    the way I currently understand the posterior update, this _shouldnt_
    cause any issues. famous last words?

--- picard/distributions/gaussian.py
+++ picard/distributions/gaussian.py
@@ -108,17 +108,37 @@ class MultivariateGaussian(Distribution):
 class NormalInverseWishart(Distribution): 
 
     def __init__(self, params : dict): 
-        self.mean = params['mean']
-        self.dof = params['dof']
+        self.prior_mean = params['mean']
+        self.posterior_mean = None 
+        self.prior_dof = params['dof']
+        self.posterior_dof = None
         self.prior_measurements = params['prior_measurements']
-        self.scale_matrix = params['scale_matrix']
+        self.posterior_measurements = None
+        self.prior_scale_matrix = params['scale_matrix']
+        self.posterior_scale_matrix = None
 
     def __repr__(self): 
         return self.__class__.__name__ + f"(mean={self.mean}, dof={self.dof}, prior measurements={self.prior_measurements}, scale matrix={self.scale_matrix}"
 
+    @property 
+    def mean(self): 
+        return self.posterior_mean if self.posterior_mean is not None else self.prior_mean
+
+    @property 
+    def dof(self): 
+        return self.posterior_dof if self.posterior_dof is not None else self.prior_dof
+
+    @property 
+    def measurements(self): 
+        return self.posterior_measurements if self.posterior_measurements is not None else self.prior_measurements
+
+    @property 
+    def scale_matrix(self): 
+        return self.posterior_scale_matrix if self.posterior_scale_matrix is not None else self.prior_scale_matrix
+
     def sample(self, size : int = 1) -> tuple: 
         covariance = InverseWishart(dict(scale_matrix=self.scale_matrix, dof=self.dof)).sample() 
-        mean = MultivariateGaussian(dict(mean=self.mean, covariance=covariance/self.prior_measurements)).sample() 
+        mean = MultivariateGaussian(dict(mean=self.mean, covariance=covariance/self.measurements)).sample() 
         return (mean, covariance)
 
     def density(self, obs : np.ndarray) -> float: 
@@ -126,5 +146,15 @@ class NormalInverseWishart(Distribution):
         inv_wishart_pdf = stats.invwishart.pdf(obs, df=self.dof, scale=self.scale_matrix)
         return normal_pdf * inv_wishart_pdf
 
-    def absorb(self, obs : np.ndarray, count : float): 
+    def absorb(self, likelihood : Distribution, obs : np.ndarray, sufficient_stats : dict): 
+        # TODO: Dangerous, be careful about using the properties like this, remember to test this later 
+        assert all(stat in list(sufficient_stats.keys()) for stat in ['scatter_matrix', 'n_measurements', 'dof', 'mean']), 'insufficient stats provided'
+        self.posterior_scale_matrix =  self.scale_matrix + sufficient_stats['scatter_matrix'] + \
+                                        ((self.measurements * sufficient_stats['n_measurements']) / (self.measurements + sufficient_stats['n_measurements'])) * \
+                                        np.outer((sufficient_stats['mean'] - self.mean), (sufficient_stats['mean'] - self.mean))
+        self.posterior_mean = (self.measurements * self.mean + sufficient_stats['n_measurements'] * sufficient_stats['mean']) / (self.measurements + sufficient_stats['n_measurements'])
+        self.posterior_dof = self.dof + sufficient_stats['n_measurements']
+        self.posterior_measurements = self.measurements + sufficient_stats['n_measurements']
+
+        # likelihood.absorb(obs, sufficient_stats)
         # TODO: how is this going to work? I think actually we should delegate the updates to the constituent distributions
